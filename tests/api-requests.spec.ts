542f327e6a5b5b0e1b8af402f6884a5b943487c6
Siddharth Gupta <siddharth7188@gmail.com>
2026-09-17 12:02:29 +0200

    updated as per review comments

--- tests/api-requests.spec.ts
+++ tests/api-requests.spec.ts
@@ -1,6 +1,4 @@
 import { test, expect, request } from '@playwright/test'
-import specialtiestoadd from '../test-data/specialties-to-add.json'
-import vettoadd from '../test-data/vet-to-add.json'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/')
@@ -9,24 +7,25 @@ test.beforeEach(async ({ page }) => {
 test('Validation of delete speciality', async ({ request, page }) => {
 
     const addSpecialtyResponse = await request.post('https://petclinic-api.bondaracademy.com/petclinic/api/specialties', {
-        data: specialtiestoadd
+        data: { "name": "api testing expert" }
     })
     expect(addSpecialtyResponse.status()).toEqual(201)
     await page.getByText('Specialties').click()
-    await expect(page.getByRole('textbox').last()).toHaveValue(specialtiestoadd.name)
-    await page.getByRole('row', { name: specialtiestoadd.name }).getByRole('button', { name: 'Delete' }).click()
-    await expect(page.getByRole('row', { name: specialtiestoadd.name })).toHaveCount(0)
+    await expect(page.getByRole('textbox').last()).toHaveValue('api testing expert')
+    await page.getByRole('row', { name: 'api testing expert' }).getByRole('button', { name: 'Delete' }).click()
+    await page.waitForResponse('**/specialties/**')
+    await expect(page.getByRole('row', { name: 'api testing expert' })).toHaveCount(0)
 })
 
 test('Add and delete veterinarian', async ({ request, page }) => {
     const vetCreatedResponse = await request.post('https://petclinic-api.bondaracademy.com/petclinic/api/vets', {
-        data: vettoadd
+        data: { "firstName": "Vet", "lastName": "ViaAPI", "id": null, "specialties": [] }
     })
     expect(vetCreatedResponse.status()).toEqual(201)
     const vetCreatedResponseBody = await vetCreatedResponse.json()
     const vetId = vetCreatedResponseBody.id
     const vetFullName = `${vetCreatedResponseBody.firstName} ${vetCreatedResponseBody.lastName}`
-    expect(vetFullName).toEqual(`${vettoadd.firstName} ${vettoadd.lastName}`)
+
     await page.getByText('Veterinarians').click()
     await page.getByRole('link', { name: 'All' }).click()
     await expect(page.getByRole('row', { name: vetFullName }).locator('td').nth(1)).toBeEmpty()
@@ -35,6 +34,8 @@ test('Add and delete veterinarian', async ({ request, page }) => {
     await page.getByRole('checkbox', { name: 'dentistry' }).check()
     await page.locator('.dropdown-display').click()
     await page.getByRole('button', { name: 'Save Vet' }).click()
+    await expect(page.getByRole('row', { name: vetFullName }).locator('td').nth(1)).toHaveText('dentistry')
+
     const deleteVetResponse = await request.delete(`https://petclinic-api.bondaracademy.com/petclinic/api/vets/${vetId}`)
     expect(deleteVetResponse.status()).toEqual(204)
     const listOfVetsResponse = await request.get('https://petclinic-api.bondaracademy.com/petclinic/api/vets')
@@ -51,8 +52,18 @@ test('New speciality is displayed', async ({ request, page }) => {
     const addSpecialtyResponseBody = await addSpecialtyResponse.json()
     const specialityId = addSpecialtyResponseBody.id
 
+
+    const getSpecialityResponse = await request.get('https://petclinic-api.bondaracademy.com/petclinic/api/specialties')
+    let surgerySpecialityId;
+    for (const currentSpecialityObject of await getSpecialityResponse.json()) {
+        if (currentSpecialityObject.name == 'surgery') {
+            surgerySpecialityId = currentSpecialityObject.id
+            break
+        }
+    }
+
     const addVetResponse = await request.post('https://petclinic-api.bondaracademy.com/petclinic/api/vets', {
-        data: { "firstName": "Vet", "lastName": "ViaAPITwo", "id": null, "specialties": [{ "id": 5920, "name": "surgery" }] }
+        data: { "firstName": "Vet", "lastName": "ViaAPITwo", "id": null, "specialties": [{ "id": surgerySpecialityId, "name": "surgery" }] }
     })
     expect(addVetResponse.status()).toEqual(201)
     const addVetResponseBody = await addVetResponse.json()
@@ -76,5 +87,6 @@ test('New speciality is displayed', async ({ request, page }) => {
     expect(deleteSpecialityResponse.status()).toEqual(204)
 
     await page.getByRole('link', { name: 'Specialties' }).click()
+    await page.waitForResponse('**/specialties')
     await expect(page.getByRole('textbox').last()).not.toHaveValue('api testing ninja')
 })
